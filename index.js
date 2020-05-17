3da7d6297deb74eb16b043d342e1d2726fef0939
Vismay <59011103+vismay-nit@users.noreply.github.com>
2020-05-17 16:42:40 +0530

    Drum file

---
+++ index.js
@@ -0,0 +1,71 @@
+var numberOfButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButton; i++) {
+
+  // Detecting Button Press
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+
+        butttonAnimation(buttonInnerHtml);
+  });
+
+}
+
+  // Detecting Keyboard Press
+document.addEventListener("keypress",function(event){
+
+  makeSound(event.key);
+
+  butttonAnimation(event.key);
+});
+
+
+function makeSound(key){
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kickbass = new Audio("kick-bass.mp3");
+      kickbass.play();
+      break;
+    case "l":
+      var snare = new Audio("snare.mp3");
+      snare.play();
+      break;
+    default:
+      console.log(buttonInnerHtml);
+
+  }
+}
+
+
+function butttonAnimation(currentKey){
+
+    var activeButton=document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+      activeButton.classList.remove("pressed");
+    },100);
+}
